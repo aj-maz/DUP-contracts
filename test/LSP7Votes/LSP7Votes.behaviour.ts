13f1b0d1d17b29ed5ef7761037a29a2d4864759f
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-22 00:23:52 +0430

    delegation without balance test added

--- test/LSP7Votes/LSP7Votes.behaviour.ts
+++ test/LSP7Votes/LSP7Votes.behaviour.ts
@@ -61,36 +61,87 @@ export const shouldBehaveLikeLSP7Votes = (
     it("delegation with balance", async function () {
       const { accounts, supply, lsp7Votes } = context;
       const { holder } = accounts;
-
       const token = lsp7Votes;
+
       await token.mint(holder.address, supply, true, "0x");
       expect(await token.delegates(holder.address)).to.be.equal(ZERO_ADDRESS);
 
-      const delegateTx = await token.connect(holder).delegate(holder.address);
+      const tx = await token.connect(holder).delegate(holder.address);
+      const receipt = await tx.wait();
 
-      expect(delegateTx)
-        .to.emit(token, "DelegateChanged")
-        .withArgs(holder.address, ZERO_ADDRESS, holder.address)
-        .to.emit(token, "DelegateVotesChanged")
-        .withArgs(holder.address, 0, supply);
+      if (receipt.events) {
+        const delegateChangeEvent = receipt.events.find(
+          (e) => e.event === "DelegateChanged"
+        );
+        expect(delegateChangeEvent).to.not.be.equal(undefined);
+        expect(delegateChangeEvent?.args?.delegator).to.be.equal(
+          holder.address
+        );
+        expect(delegateChangeEvent?.args?.fromDelegate).to.be.equal(
+          ZERO_ADDRESS
+        );
+        expect(delegateChangeEvent?.args?.toDelegate).to.be.equal(
+          holder.address
+        );
+        const delegateVotesChangeEvent = receipt.events.find(
+          (e) => e.event === "DelegateVotesChanged"
+        );
+        expect(delegateVotesChangeEvent).to.not.be.equal(undefined);
+        expect(delegateVotesChangeEvent?.args?.delegate).to.be.equal(
+          holder.address
+        );
+        expect(delegateVotesChangeEvent?.args?.previousBalance).to.be.equal(
+          String(0)
+        );
+        expect(delegateVotesChangeEvent?.args?.newBalance).to.be.equal(
+          String(supply)
+        );
+      }
 
       expect(await token.delegates(holder.address)).to.be.equal(holder.address);
       expect(String(await token.getVotes(holder.address))).to.be.equal(
         String(supply)
       );
-      if (delegateTx && delegateTx.blockNumber) {
+      if (tx && tx.blockNumber) {
         expect(
-          String(
-            await token.getPastVotes(holder.address, delegateTx.blockNumber - 1)
-          )
+          String(await token.getPastVotes(holder.address, tx.blockNumber - 1))
         ).to.be.equal("0");
         await network.provider.send("evm_mine");
         expect(
-          String(
-            await token.getPastVotes(holder.address, delegateTx.blockNumber)
-          )
+          String(await token.getPastVotes(holder.address, tx.blockNumber))
         ).to.be.equal(String(supply));
       }
+    });
+
+    it("delegation without balance", async function () {
+      const { accounts, lsp7Votes } = context;
+      const { holder } = accounts;
+      const token = lsp7Votes;
+      expect(await token.delegates(holder.address)).to.be.equal(ZERO_ADDRESS);
+      const tx = await token.connect(holder).delegate(holder.address);
+      const receipt = await tx.wait();
+
+      if (receipt.events) {
+        const delegateChangeEvent = receipt.events.find(
+          (e) => e.event === "DelegateChanged"
+        );
+        expect(delegateChangeEvent).to.not.be.equal(undefined);
+        expect(delegateChangeEvent?.args?.delegator).to.be.equal(
+          holder.address
+        );
+        expect(delegateChangeEvent?.args?.fromDelegate).to.be.equal(
+          ZERO_ADDRESS
+        );
+        expect(delegateChangeEvent?.args?.toDelegate).to.be.equal(
+          holder.address
+        );
+        const delegateVotesChangeEvent = receipt.events.find(
+          (e) => e.event === "DelegateVotesChanged"
+        );
+        expect(delegateVotesChangeEvent).to.be.equal(undefined);
+      }
+      
+      expect(await token.delegates(holder.address)).to.be.equal(holder.address);
     });
   });
 };
