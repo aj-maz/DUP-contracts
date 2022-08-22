2bcc8e50af71b4e33e3b834274f7bfb820363caa
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-22 14:37:45 +0430

    when transfering tests added

--- test/LSP7Votes/LSP7Votes.behaviour.ts
+++ test/LSP7Votes/LSP7Votes.behaviour.ts
@@ -7,9 +7,9 @@ import { LSP7VotesMock } from "../../typechain-types/contracts/mocks/LSP7VotesMo
 
 export type LSP7VotesTestAccounts = {
   holder: SignerWithAddress;
-  recepient: SignerWithAddress;
+  recipient: SignerWithAddress;
   holderDelegatee: SignerWithAddress;
-  recepientDelegatee: SignerWithAddress;
+  recipientDelegatee: SignerWithAddress;
   anyone1: SignerWithAddress;
   anyone2: SignerWithAddress;
 };
@@ -17,17 +17,17 @@ export type LSP7VotesTestAccounts = {
 export const getNamedAccounts = async (): Promise<LSP7VotesTestAccounts> => {
   const [
     holder,
-    recepient,
+    recipient,
     holderDelegatee,
-    recepientDelegatee,
+    recipientDelegatee,
     anyone1,
     anyone2,
   ] = await ethers.getSigners();
   return {
     holder,
-    recepient,
+    recipient,
     holderDelegatee,
-    recepientDelegatee,
+    recipientDelegatee,
     anyone1,
     anyone2,
   };
@@ -67,36 +67,14 @@ export const shouldBehaveLikeLSP7Votes = (
       expect(await token.delegates(holder.address)).to.be.equal(ZERO_ADDRESS);
 
       const tx = await token.connect(holder).delegate(holder.address);
-      const receipt = await tx.wait();
 
-      if (receipt.events) {
-        const delegateChangeEvent = receipt.events.find(
-          (e) => e.event === "DelegateChanged"
-        );
-        expect(delegateChangeEvent).to.not.be.equal(undefined);
-        expect(delegateChangeEvent?.args?.delegator).to.be.equal(
-          holder.address
-        );
-        expect(delegateChangeEvent?.args?.fromDelegate).to.be.equal(
-          ZERO_ADDRESS
-        );
-        expect(delegateChangeEvent?.args?.toDelegate).to.be.equal(
-          holder.address
-        );
-        const delegateVotesChangeEvent = receipt.events.find(
-          (e) => e.event === "DelegateVotesChanged"
-        );
-        expect(delegateVotesChangeEvent).to.not.be.equal(undefined);
-        expect(delegateVotesChangeEvent?.args?.delegate).to.be.equal(
-          holder.address
-        );
-        expect(delegateVotesChangeEvent?.args?.previousBalance).to.be.equal(
-          String(0)
-        );
-        expect(delegateVotesChangeEvent?.args?.newBalance).to.be.equal(
-          String(supply)
-        );
-      }
+      await expect(tx)
+        .to.emit(token, "DelegateChanged")
+        .withArgs(holder.address, ZERO_ADDRESS, holder.address);
+
+      await expect(tx)
+        .to.emit(token, "DelegateVotesChanged")
+        .withArgs(holder.address, String(0), String(supply));
 
       expect(await token.delegates(holder.address)).to.be.equal(holder.address);
       expect(String(await token.getVotes(holder.address))).to.be.equal(
@@ -119,29 +97,222 @@ export const shouldBehaveLikeLSP7Votes = (
       const token = lsp7Votes;
       expect(await token.delegates(holder.address)).to.be.equal(ZERO_ADDRESS);
       const tx = await token.connect(holder).delegate(holder.address);
+
+      await expect(tx)
+        .to.emit(token, "DelegateChanged")
+        .withArgs(holder.address, ZERO_ADDRESS, holder.address);
+
+      await expect(tx).to.not.emit(token, "DelegateVotesChanged");
+
+      expect(await token.delegates(holder.address)).to.be.equal(holder.address);
+    });
+  });
+
+  describe("When changing delegation", () => {
+    beforeEach(async function () {
+      const { accounts, lsp7Votes, supply } = context;
+      const { holder } = accounts;
+      const token = lsp7Votes;
+
+      await token.mint(holder.address, supply, true, "0x");
+      await token.connect(holder).delegate(holder.address);
+    });
+
+    it("call", async function () {
+      const { accounts, lsp7Votes, supply } = context;
+      const { holder, holderDelegatee } = accounts;
+      const token = lsp7Votes;
+
+      expect(await token.delegates(holder.address)).to.be.equal(holder.address);
+
+      const tx = await token.connect(holder).delegate(holderDelegatee.address);
       const receipt = await tx.wait();
 
-      if (receipt.events) {
-        const delegateChangeEvent = receipt.events.find(
-          (e) => e.event === "DelegateChanged"
+      await expect(tx)
+        .to.emit(token, "DelegateChanged")
+        .withArgs(holder.address, holder.address, holderDelegatee.address);
+      await expect(tx)
+        .to.emit(token, "DelegateVotesChanged")
+        .withArgs(holder.address, String(supply), String(0));
+      await expect(tx)
+        .to.emit(token, "DelegateVotesChanged")
+        .withArgs(holderDelegatee.address, String(0), String(supply));
+
+      expect(await token.delegates(holder.address)).to.be.equal(
+        holderDelegatee.address
+      );
+
+      expect(String(await token.getVotes(holder.address))).to.be.equal("0");
+      expect(String(await token.getVotes(holderDelegatee.address))).to.be.equal(
+        String(supply)
+      );
+      expect(
+        String(
+          await token.getPastVotes(holder.address, receipt.blockNumber - 1)
+        )
+      ).to.be.equal(String(supply));
+      expect(
+        String(
+          await token.getPastVotes(
+            holderDelegatee.address,
+            receipt.blockNumber - 1
+          )
+        )
+      ).to.be.equal("0");
+      await network.provider.send("evm_mine");
+      expect(
+        String(await token.getPastVotes(holder.address, receipt.blockNumber))
+      ).to.be.equal("0");
+      expect(
+        String(
+          await token.getPastVotes(holderDelegatee.address, receipt.blockNumber)
+        )
+      ).to.be.equal(String(supply));
+    });
+  });
+
+  describe("When transfering", () => {
+    beforeEach(async function () {
+      const { accounts, lsp7Votes, supply } = context;
+      const { holder } = accounts;
+      const token = lsp7Votes;
+
+      await token.mint(holder.address, supply, true, "0x");
+    });
+
+    it("no delegation", async function () {
+      const { accounts, lsp7Votes: token } = context;
+      const { holder, recipient } = accounts;
+
+      const tx = await token
+        .connect(holder)
+        .transfer(holder.address, recipient.address, 1, true, "0x");
+
+      await expect(tx)
+        .to.emit(token, "Transfer")
+        .withArgs(
+          holder.address,
+          holder.address,
+          recipient.address,
+          String(1),
+          true,
+          "0x"
         );
-        expect(delegateChangeEvent).to.not.be.equal(undefined);
-        expect(delegateChangeEvent?.args?.delegator).to.be.equal(
-          holder.address
+
+      await expect(tx).to.not.emit(token, "DelegateVotesChanged");
+
+      this.holderVotes = "0";
+      this.recipientVotes = "0";
+    });
+
+    it("sender delegation", async function () {
+      const { accounts, lsp7Votes: token, supply } = context;
+      const { holder, recipient } = accounts;
+
+      await token.connect(holder).delegate(holder.address);
+      const tx = await token
+        .connect(holder)
+        .transfer(holder.address, recipient.address, 1, true, "0x");
+
+      await expect(tx)
+        .to.emit(token, "Transfer")
+        .withArgs(
+          holder.address,
+          holder.address,
+          recipient.address,
+          String(1),
+          true,
+          "0x"
         );
-        expect(delegateChangeEvent?.args?.fromDelegate).to.be.equal(
-          ZERO_ADDRESS
+      await expect(tx)
+        .to.emit(token, "DelegateVotesChanged")
+        .withArgs(holder.address, supply, supply.sub(1));
+
+      this.holderVotes = supply.sub(1);
+      this.recipientVotes = "0";
+    });
+
+    it("receiver delegation", async function () {
+      const { accounts, lsp7Votes: token, supply } = context;
+      const { holder, recipient } = accounts;
+
+      await token.connect(holder).delegate(recipient.address);
+      const tx = await token
+        .connect(holder)
+        .transfer(holder.address, recipient.address, 1, true, "0x");
+
+      await expect(tx)
+        .to.emit(token, "Transfer")
+        .withArgs(
+          holder.address,
+          holder.address,
+          recipient.address,
+          String(1),
+          true,
+          "0x"
         );
-        expect(delegateChangeEvent?.args?.toDelegate).to.be.equal(
-          holder.address
+      await expect(tx)
+        .to.emit(token, "DelegateVotesChanged")
+        .withArgs(recipient.address, supply, supply.sub(1));
+
+      this.holderVotes = "0";
+      this.recipientVotes = supply.sub(1);
+    });
+
+    it("full delegation", async function () {
+      const { accounts, lsp7Votes: token, supply } = context;
+      const { holder, recipient } = accounts;
+
+      await token.connect(holder).delegate(holder.address);
+      await token.connect(recipient).delegate(recipient.address);
+
+      const tx = await token
+        .connect(holder)
+        .transfer(holder.address, recipient.address, 1, true, "0x");
+
+      await expect(tx)
+        .to.emit(token, "Transfer")
+        .withArgs(
+          holder.address,
+          holder.address,
+          recipient.address,
+          String(1),
+          true,
+          "0x"
         );
-        const delegateVotesChangeEvent = receipt.events.find(
-          (e) => e.event === "DelegateVotesChanged"
-        );
-        expect(delegateVotesChangeEvent).to.be.equal(undefined);
-      }
-      
-      expect(await token.delegates(holder.address)).to.be.equal(holder.address);
+      await expect(tx)
+        .to.emit(token, "DelegateVotesChanged")
+        .withArgs(holder.address, supply, supply.sub(1));
+
+      await expect(tx)
+        .to.emit(token, "DelegateVotesChanged")
+        .withArgs(recipient.address, 0, 1);
+
+      this.holderVotes = supply.sub(1);
+      this.recipientVotes = "1";
+    });
+
+    afterEach(async function () {
+      const { accounts, lsp7Votes: token, supply } = context;
+      const { holder, recipient } = accounts;
+
+      expect(String(await token.getVotes(holder.address))).to.be.equal(
+        this.holderVotes
+      );
+      expect(String(await token.getVotes(recipient.address))).to.be.equal(
+        this.recipientVotes
+      );
+
+      // need to advance 2 blocks to see the effect of a transfer on "getPastVotes"
+      const blockNumber = await ethers.provider.getBlock("latest");
+      await network.provider.send("evm_mine");
+
+      expect(
+        String(await token.getPastVotes(holder.address, blockNumber.number))
+      ).to.be.equal(String(this.holderVotes));
+      expect(
+        String(await token.getPastVotes(recipient.address, blockNumber.number))
+      ).to.be.equal(String(this.recipientVotes));
     });
   });
 };
