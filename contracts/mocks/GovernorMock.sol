ee27056142d1f428a7560c6ed63abb9153d4108b
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-24 20:15:43 +0430

    added governor mock

---
+++ contracts/mocks/GovernorMock.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../governance/Governor.sol";
+import "../governance/GovernorCore.sol";
+import "../governance/extensions/GovernorSettings.sol";
+import "../governance/extensions/GovernorSettingsCore.sol";
+import "../governance/extensions/GovernorCountingSimple.sol";
+import "../governance/extensions/GovernorVotesQuorumFraction.sol";
+import "../governance/extensions/GovernorVotes.sol";
+import "../LSP7Votes/ILSP7Votes.sol";
+
+contract GovernorMock is
+    GovernorSettings,
+    GovernorVotesQuorumFraction,
+    GovernorCountingSimple,
+    Governor
+{
+    constructor(
+        address target_,
+        address up_,
+        string memory name_,
+        ILSP7Votes token_,
+        uint256 votingDelay_,
+        uint256 votingPeriod_,
+        uint256 quorumNumerator_
+    )
+        Governor(target_, up_, name_)
+        GovernorSettings(votingDelay_, votingPeriod_, 0)
+        GovernorVotes(token_)
+        GovernorVotesQuorumFraction(quorumNumerator_)
+    {}
+
+    function cancel(
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 salt
+    ) public returns (uint256 proposalId) {
+        return _cancel(values, calldatas, salt);
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(GovernorCore, GovernorSettingsCore)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+
+    function propose(
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) public virtual override(GovernorCore) returns (uint256) {
+        return super.propose(values, calldatas, description);
+    }
+}
